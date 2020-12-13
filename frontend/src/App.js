3a92c3184aeaa25901a9fdc4255105f11e88674a
David Huang <huang.liwei.17@gmail.com>
2020-12-13 11:43:24 -0500

    Add react router and react router bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header></Header>
       <main>
         <Container className="py-3">
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
         </Container>
       </main>
       <Footer></Footer>
-    </>
+    </Router>
   );
 };
 
