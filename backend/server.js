8bda7e9ab231edfde48fae9e24ea1e738c32e057
David Huang <huang.liwei.17@gmail.com>
2020-12-14 00:41:33 -0500

    Add in mongoose to connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 // Common JS
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -24,5 +28,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
 );
